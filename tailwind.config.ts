3b376678c9de262bd584a0e70440cb89e8ae6754
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-06-03 22:37:37 -0300

    project base config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,16 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {},
-  plugins: [],
-};
-export default config;
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        fontFamily: {
+            sans: 'var(--font-inter)',
+        }
+    },
+    plugins: [],
+}
+export default config
