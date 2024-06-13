f02d1c0855f830af6a4e4f247f35b219afc24bbb
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-06-12 22:50:10 -0300

    cache introduction

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -5,7 +5,11 @@ import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
 
 async function getFeaturedProducts(): Promise<Product[]> {
-    const response = await api('/products/featured')
+    const response = await api('/products/featured', {
+        next: {
+            revalidate: 60 * 60
+        }
+    })
     return await response.json()
 }
 
