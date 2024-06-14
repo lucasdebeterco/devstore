2a33e31a193833f30bd56090456955e4c37319ba
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-06-13 22:59:20 -0300

    skeleton loading screen

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,3 +1,11 @@
+import { Skeleton } from '@/app/components/skeleton'
+
 export default function HomeLoading() {
-    return <p>Carregando...</p>
+    return (
+        <div className="grid max-h-full grid-cols-9 grid-rows-6 gap-6">
+            <Skeleton className="col-span-6 row-span-6 h-[856px]" />
+            <Skeleton className="col-span-3 row-span-3" />
+            <Skeleton className="col-span-3 row-span-3" />
+        </div>
+    )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,8 +7,10 @@ const config: Config = {
         './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     ],
     theme: {
-        fontFamily: {
-            sans: 'var(--font-inter)',
+        extend: {
+            fontFamily: {
+                sans: 'var(--font-inter)',
+            }
         }
     },
     plugins: [],
