00cc8f0d26fd6bb760496dc6ed094ede66ec6617
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-06-16 00:28:44 -0300

    product data fetch

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -1,11 +1,32 @@
 import Image from 'next/image'
 
-export default function Product() {
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+
+interface ProductProps {
+    params: {
+        slug: string
+    }
+}
+
+async function getProduct(slug: string): Promise<Product> {
+    const response = await api(`/products/${slug}`, {
+        next: {
+            revalidate: 60 * 60
+        }
+    })
+    return await response.json()
+}
+
+
+export default async function ProductPage({ params }: ProductProps) {
+    const product = await getProduct(params.slug)
+
     return (
         <div className="relative grid max-h-[860px] grid-cols-3">
             <div className="col-span-2 overflow-hidden">
                 <Image
-                    src="/moletom-never-stop-learning.png"
+                    src={product.image}
                     alt=""
                     width={1000}
                     height={1000}
@@ -15,19 +36,26 @@ export default function Product() {
 
             <div className="flex flex-col justify-center px-12">
                 <h1 className="text-3xl font-bold leading-tight">
-                    Moletom Never Stop Learning
+                    {product.title}
                 </h1>
 
                 <p className="mt-2 leading-relaxed text-zinc-400">
-                    Moletom Never Stop Learning
+                    {product.description}
                 </p>
 
                 <div className="mt-8 flex items-center gap-3">
                     <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5 font-semibold">
-                            R$ 129
+                        {product.price.toLocaleString('en-US', {
+                            style: 'currency',
+                            currency: 'USD',
+                            maximumFractionDigits: 0
+                        })}
                     </span>
                     <span className="text-sm text-zinc-400">
-                            Em 12x s/juros de R$ 10,75
+                        In 12 parcels with no interests of {(product.price / 12).toLocaleString('en-US', {
+                            style: 'currency',
+                            currency: 'USD'
+                        })}
                     </span>
                 </div>
 
