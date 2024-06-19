1b39bff06164f97f662a4148773c8a8aca95c774
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-06-19 14:32:06 -0300

    creating cart provider and addToCart function

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,12 +1,15 @@
 import { ReactNode } from 'react'
 
 import { Header } from '@/app/components/header'
+import { CartProvider } from '@/contexts/cart-context'
 
 export default function StoreLayout({children}: { children: ReactNode }) {
     return (
-        <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
-            <Header />
-            {children}
-        </div>
+        <CartProvider>
+            <div className="mx-auto grid min-h-screen w-full max-w-[1600px] grid-rows-[min-content_max-content] gap-5 p-8">
+                <Header />
+                {children}
+            </div>
+        </CartProvider>
     )
 }
