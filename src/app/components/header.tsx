38db26048fdfe4a4f4275b6fb5e46ba9efa92732
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-06-22 23:52:30 -0300

    next navigation

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,8 +1,8 @@
-import { Search, ShoppingBag } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 
 import { CartWidget } from '@/app/components/cart-widget'
+import { SearchForm } from '@/app/components/search-form'
 
 export function Header() {
     return (
@@ -11,14 +11,8 @@ export function Header() {
                 <Link href="/" className="text-2xl font-extrabold text-white">
                     Devstore
                 </Link>
-                <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-offset-zinc-700">
-                    <Search className="size-5 text-zinc-500" />
 
-                    <input
-                        placeholder="Search products..."
-                        className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-                    />
-                </form>
+                <SearchForm />
             </div>
             <div className="flex items-center gap-4">
                 <CartWidget />
