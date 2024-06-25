eea4f9da70abc218f8579bf7c34d68e6c645a92b
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-06-25 00:01:22 -0300

    listing products from api

--- src/app/(store)/search/page.tsx
+++ src/app/(store)/search/page.tsx
@@ -2,10 +2,23 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+
 interface SearchProps {
     searchParams: {
         q: string
     }
+}
+
+async function searchProducts(query: string): Promise<Product[]> {
+    const response = await api( `/products/search?q=${query}`, {
+        next: {
+            revalidate: 60 * 60
+        }
+    })
+
+    return await response.json()
 }
 
 export default async function Search({ searchParams }: SearchProps) {
@@ -15,6 +28,8 @@ export default async function Search({ searchParams }: SearchProps) {
         redirect('/')
     }
 
+    const products = await searchProducts(query)
+
     return (
         <div className="flex flex-col gap-4">
             <p className="text-sm">
@@ -22,31 +37,36 @@ export default async function Search({ searchParams }: SearchProps) {
             </p>
 
             <div className="grid grid-cols-3 gap-6">
-                <Link
-                    href={`/product/moletom-never-stop-learning`}
-                    className="group relative flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
-                >
-                    <Image
-                        src="/moletom-never-stop-learning.png"
-                        width={480}
-                        height={480}
-                        quality={100}
-                        alt=""
-                        className="transition-transform duration-500 group-hover:scale-105"
-                    />
+                {products.map(product => (
+                    <Link
+                        key={product.id}
+                        href={`/product/${product.slug}`}
+                        className="group relative flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+                    >
+                        <Image
+                            src={product.image}
+                            width={480}
+                            height={480}
+                            quality={100}
+                            alt=""
+                            className="transition-transform duration-500 group-hover:scale-105"
+                        />
 
-                    <div
-                        className="absolute bottom-8 right-8 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
-                        <span className="truncate text-sm">Moletom</span>
-                        <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-                            {Number(129).toLocaleString('en-US', {
-                                style: 'currency',
-                                currency: 'USD',
-                                maximumFractionDigits: 0
-                            })}
-                        </span>
-                    </div>
-                </Link>
+                        <div
+                            className="absolute bottom-8 right-8 flex h-12 max-w-[280px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+                            <span className="truncate text-sm">
+                                {product.title}
+                            </span>
+                            <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+                                {product.price.toLocaleString('en-US', {
+                                    style: 'currency',
+                                    currency: 'USD',
+                                    maximumFractionDigits: 0
+                                })}
+                            </span>
+                        </div>
+                    </Link>
+                ))}
             </div>
         </div>
     )
