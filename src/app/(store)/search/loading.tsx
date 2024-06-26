a7eb369bf59546972b8ee00c5fe39e8122eac470
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-06-26 00:07:28 -0300

    search page loading

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,27 @@
+'use client'
+
+import { useSearchParams } from 'next/navigation'
+
+import { Skeleton } from '@/app/components/skeleton'
+
 export default function SearchLoading() {
-    return <p>Loading search...</p>
+    const searchParams = useSearchParams()
+    const query = searchParams.get('q')
+
+    return (
+        <div className="flex flex-col gap-4">
+            <p className="text-sm">
+                Results for: <span className="font-semibold">{query ?? ''}</span>
+            </p>
+
+            <div className="grid grid-cols-3 gap-6">
+                <Skeleton className="h-[400px]" />
+                <Skeleton className="h-[400px]" />
+                <Skeleton className="h-[400px]" />
+                <Skeleton className="h-[400px]" />
+                <Skeleton className="h-[400px]" />
+                <Skeleton className="h-[400px]" />
+            </div>
+        </div>
+    )
 }
