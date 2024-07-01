4d07575ef819c9465003f3530daa4194ae729105
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-07-01 16:42:58 -0300

    next config lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        ignoreBuildErrors: true
+    },
+    typescript: {
+        ignoreBuildErrors: true
+    },
     images: {
         domains: ['github.com']
     }
