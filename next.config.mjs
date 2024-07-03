6e70e9a690aa31da64aaa722e070055cf07f9f4c
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-07-03 16:51:56 -0300

    next config fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     eslint: {
-        ignoreBuildErrors: true
+        ignoreDuringBuilds: true
     },
     typescript: {
         ignoreBuildErrors: true
