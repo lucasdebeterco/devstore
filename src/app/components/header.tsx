3fd5b5b82132b2173ed892061f7e38a463c51cbc
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-07-04 17:10:33 -0300

    fix searchParams error

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { Suspense } from 'react'
 
 import { CartWidget } from '@/app/components/cart-widget'
 import { SearchForm } from '@/app/components/search-form'
@@ -12,7 +13,9 @@ export function Header() {
                     Devstore
                 </Link>
 
-                <SearchForm />
+                <Suspense fallback={null}>
+                    <SearchForm />
+                </Suspense>
             </div>
             <div className="flex items-center gap-4">
                 <CartWidget />
