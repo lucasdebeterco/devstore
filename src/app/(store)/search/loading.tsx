3e9f1d3152213c648a0835fa6b5d3f5eb43a8a24
Lucas Wamser Rosa Debeterco <lucasdebeterco@gmail.com>
2024-07-05 17:12:16 -0300

    fix searchParams error loading

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,17 +1,12 @@
 'use client'
 
-import { useSearchParams } from 'next/navigation'
-
 import { Skeleton } from '@/app/components/skeleton'
 
 export default function SearchLoading() {
-    const searchParams = useSearchParams()
-    const query = searchParams.get('q')
-
     return (
         <div className="flex flex-col gap-4">
             <p className="text-sm">
-                Results for: <span className="font-semibold">{query ?? ''}</span>
+                Results for:
             </p>
 
             <div className="grid grid-cols-3 gap-6">
